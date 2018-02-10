223c10e6bf8e88eafc7a633e853020bf37d2ced0
Antonia REID <areid@e1r3p1.42.fr>
2018-02-10 14:38:52 +0100

    Adding

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 12:27:49 by areid             #+#    #+#             */
-/*   Updated: 2018/02/04 15:00:09 by areid            ###   ########.fr       */
+/*   Updated: 2018/02/10 14:16:45 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 # define FT_PRINTF_H
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
+//#include "libft/libft.a"
+
+typedef struct		s_args
+{
+	char id;
+	char flag;
+	char *value;
+}					t_args;
+
+
+int	ft_atoi(const char *s);   //remove when have lib
+char *ft_strnew(size_t size); //////remove later
+char *ft_strdup(const char *src);
 
 #endif
--- ft_printf_struct.c
+++ ft_printf_struct.c
@@ -6,51 +6,73 @@
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 12:27:21 by areid             #+#    #+#             */
-/*   Updated: 2018/02/04 15:00:56 by areid            ###   ########.fr       */
+/*   Updated: 2018/02/10 14:37:51 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);
+		write(1, &c, 1);
 }
 
 void	ft_print_str(char *str)
 {
-	while (str && *str != '\0')
-	{
-		ft_putchar(*str);
-		str++;
-	}
+		while (str && *str != '\0')
+		{
+				ft_putchar(*str);
+				str++;
+		}
 }
 
 void	ft_printf(char *str, ...)
 {
-	int i;
+		int i;
+		int	argnum;
+		va_list argptr;
+		t_args *arg;
 
-	i = 0;
-	while (str && str[i] != '\0')
-	{
-		if (str[i] == '%')
+		va_start(argptr, str);
+		i = 0;
+		argnum = 0;
+		while (str && str[i] != '\0')
 		{
-			i++;
+				if (str[i] == '%')
+						argnum++;
+
+				i++;
 		}
-		i++;
-	}
-	ft_print_str(str);
+		
+		printf("num of args is %d\n", argnum);
+		i = 0;
+		arg = (t_args*)ft_strnew(argnum + 1);
+		while (i <= argnum)
+		{
+				arg[i].value = ft_strdup((char *)str);
+				printf("\ni is %d\n", i);
+				printf("ret[%d].value is %s", i, arg[i].value);
+				str = va_arg(argptr, char *);
+				i++;
+		}
+		va_end(argptr);
 }
 
 int main()
 {
-	int		num;
+		int		num;
+char *lel = "lelstr";		
+		char *dog = "dogstr";
+		char *cat = "catstr";
+		char *hii = "hiistr";
 
-	num = 2;
-	printf("Me\nComp\n\n");
-//	ft_printf("HELLO\n%d");
-	ft_printf("HELLO %d\n", num);
-	printf("HELLO %d\n", num);
-	return (0);
+
+		num = 2;
+		printf("Me\nComp\n\n");
+		//	ft_printf("HELLO\n%d");
+		ft_printf("HELLO %s %s %s %d\n", hii, cat, dog, lel);
+		printf("\n\nHELLO %d\n", num);
+		return (0);
 }
