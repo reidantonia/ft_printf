a500a61e28e1440752ebb900c2ce39855e89ff56
Antonia REID <areid@e1r3p3.42.fr>
2018-02-10 22:45:15 +0100

    adding

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 12:27:49 by areid             #+#    #+#             */
-/*   Updated: 2018/02/10 14:16:45 by areid            ###   ########.fr       */
+/*   Updated: 2018/02/10 22:03:28 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,22 @@
 
 typedef struct		s_args
 {
-	char id;
-	char flag;
-	char *value;
+	char	id;
+	char	flag;
+	int		code_length;
+	char	value_length;
+	char	*value;
+	int		width;
+	char	*align;
+	char	*zero_fill;
 }					t_args;
 
 
-int	ft_atoi(const char *s);   //remove when have lib
-char *ft_strnew(size_t size); //////remove later
-char *ft_strdup(const char *src);
+int		ft_atoi(const char *s);   //remove when have lib
+char	*ft_strnew(size_t size); //////remove latr
+char	*ft_strdup(const char *src);
+int		ft_isdigit(int c);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
 
 #endif
--- ft_printf_red.c
+++ ft_printf_red.c
@@ -6,7 +6,7 @@
 /*   By: areid <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 12:27:21 by areid             #+#    #+#             */
-/*   Updated: 2018/02/10 18:49:07 by areid            ###   ########.fr       */
+/*   Updated: 2018/02/10 22:13:17 by areid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,32 @@ void	ft_print_str(char *str)
 	}
 }
 
-void	ft_printer(int argnum, t_args *arg)
+t_args		ft_process(char *str) 
 {
+	t_args	arg;
 	int i;
-	int arg_count;
-	char *toprint;
 
 	i = 0;
-	while (arg[0].value[i] != '\0' && i < argnum)
+	if (str[0] == '-')
+		arg.align = "left";
+	else if (str[0] == '0')
+		arg.zero_fill = "yes";
+	if (ft_isdigit(str[i]) && str[i] != '0')
 	{
-		if (arg[0].value[i] != '%')
-			to_print = ft_strj
-		if (arg[0].value[i] == '%')
-		{
-			arg_count++;
-		}
-		i++;
+		while (ft_isdigit(str[i]))
+			i++;
+		arg.width = atoi(ft_strsub(str, 0, i));
 	}
-	printf("\nsecond print %s\n\n", arg[0].value);
+	//arg.code_length = i;
+	return (arg);
 }
 
 void	ft_printf(char *str, ...)
 {
-	int		i;
+	int			i;
 	int			argnum;
 	va_list		argptr;
 	t_args		*arg;
-	char		*to_print;
 
 	va_start(argptr, str);
 	i = 0;
@@ -66,34 +65,36 @@ void	ft_printf(char *str, ...)
 		i++;
 	}
 	printf("num of args is %d\n", argnum);
-	i = 0;
 	arg = (t_args*)ft_strnew(argnum + 1);
-	while (i <= argnum)
+	i = 0;
+	argnum = 0;
+	while (str && str[i] != '\0')
 	{
-		if (i == 0)
-			printf("FIRST VALUE IS %d\n", str);
-		arg[i].value = ft_strdup((char *)str);
-		printf("\ni is %d\n", i);
-		printf("ret[%d].value is %s", i, arg[i].value);
-		str = va_arg(argptr, char *);
+		while (str && str[i] != '\0' && str[i] != '%')
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+		if (str[i] == '%')
+		{
+		argnum++;
+		arg[argnum] = ft_process(ft_strsub(str, i, ft_strlen(str)));
+		i = i + arg[argnum].code_length;
+		}
 		i++;
 	}
-	va_end(argptr);
-//	ft_printer(argnum, arg);
 }
 
 int main()
 {
 	int		num;
-	char *lel = "lelstr";		
+	char *lel = "lelstr";
 	char *dog = "dogstr";
 	char *cat = "catstr";
 	char *hii = "hiistr";
 
 	num = 2;
-	printf("Me\nComp\n\n");
-	//	ft_printf("HELLO\n%d");
-	ft_printf("HELLO %s %s %s %s\n", hii, cat, dog, lel);
-	printf("\n\nHELLO %d\n", num);
+	ft_printf("ME:HELLO %s %s %s %s", hii, cat, dog, lel);
+	printf("\nCOMPUTER:HELLO %d\n", num);
 	return (0);
 }
